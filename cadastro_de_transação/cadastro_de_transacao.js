40c21bb912969f024e7e195516c4bea7b0e7aaf5
vinicruz13 <vinicruz404@gmail.com>
2021-07-22 12:05:01 -0400

    cadastro_de_transacao.js

--- cadastro_de_transação/cadastro_de_transacao.js
+++ cadastro_de_transação/cadastro_de_transacao.js
@@ -1,9 +1,10 @@
-function onsubmit() {    var conta = document.querySelector("#conta")
-var tipoTransação = document.querySelector("#transação")
-var formaPagamento = document.querySelector("#pagamento")
-var dataEhora = document.querySelector("#datetime")
-var valor = document.querySelector("#text")
-var descrição = document.querySelector("#textarea")
+function onsubmit() {
+var conta = document.querySelector("#conta").value;
+var tipoTransação = document.querySelector("#transação").value;
+var formaPagamento = document.querySelector("#pagamento").value;
+var dataEhora = document.querySelector("#datetime").value;
+var valor = document.querySelector("#text").value;
+var descrição = document.querySelector("#textarea").value;
 
 console.log(tipoTransação, formaPagamento, dataEhora, valor, descrição)
 }
