32ecf82ea22789daf0543ed13d40b0369261829d
vinicruz13 <vinicruz404@gmail.com>
2021-07-25 10:52:56 -0400

    criação do arquivo .js

---
+++ tela_de_login/tela_de_login.js
@@ -0,0 +1,11 @@
+window.onload = function(){
+  
+}
+function onSubmit(){
+    const user = {};
+    user.email = document.querySelector("#email").value;
+    user.senha = document.querySelector("#senha").value;
+
+
+    console.log(user);
+}
