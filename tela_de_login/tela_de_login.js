be60b76e02aad21e434c8ce52cbd2f95edc09ee4
vinicruz13 <vinicruz404@gmail.com>
2021-08-08 11:24:12 -0400

    function logar adicionada

--- tela_de_login/tela_de_login.js
+++ tela_de_login/tela_de_login.js
@@ -1,8 +1,8 @@
-function onSubmit(){
+function logar(form){
     const user = {};
+
     user.email = document.querySelector("#email").value;
     user.senha = document.querySelector("#senha").value;
-
 
     console.log(user);
 }
