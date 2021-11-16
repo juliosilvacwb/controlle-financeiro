d3d7e65d82067f360de5689d12d72d2dd76656ec
Thiago Paes] <t.paes@hotmail.com>
2021-11-16 19:50:28 -0300

    Alterações cadastro de categoria

--- cadastro-de-categoria/categoria.js
+++ cadastro-de-categoria/categoria.js
@@ -1,3 +1,4 @@
+/*
 
 $(document).ready(function(){
     $("#pagrecval").mask("999.999.990,00", {reverse: true})
@@ -12,4 +13,45 @@ function onSubmit(){
     user.data = document.querySelector("#datapag").value;
 
     console.log(user);
+}
+
+*/
+// const { URL_SERVER } = require("../constants.js");
+
+async function onSubmit() {
+  const categoria = {}
+  categoria.tipdecategoria = document.querySelector('#tipdecategoria').value
+  categoria.entradaousaida = document.querySelector('#entradaousaida').value
+  categoria.formadepagamento = document.querySelector('#formadepagamento').value
+  // user.dataNascimento = document.querySelector("#date").value;
+  // user.fone = document.querySelector("#number").value;
+  // user.aceite = document.querySelector("#checkbox").checked;
+
+  save(categoria)
+}
+
+async function save(categoria) {
+  const url = 'http://18.218.201.243:8080'.concat('/category')
+  //const url = URL_SERVER.concat(CATEGORIES);
+
+  const headers = {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+
+  const properties = {
+    method: 'POST',
+    body: JSON.stringify(categoria),
+    headers
+  }
+
+  fetch(url, properties)
+    .then(res => {
+      if (res.ok) {
+        alert('Categoria cadastrada com sucesso!')
+      } else {
+        alert('Erro ao salvar Categoria!')
+      }
+    })
+    .catch(err => console.log(err))
 }
